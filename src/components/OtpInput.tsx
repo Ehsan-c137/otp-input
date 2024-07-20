3f9e43852b3cc2a173339be6039248406036133a
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-20 16:24:01 +0330

    add onChange & keyDown event

--- src/components/OtpInput.tsx
+++ src/components/OtpInput.tsx
@@ -1,13 +1,82 @@
+import "./OtpInput.module.css";
+import { useMemo } from "react";
+
 type Props = {
    valueLength: number;
    value: string;
    onChange: (value: string) => void;
 };
 
+const RE_DIGIT = new RegExp(/^\d+$/);
+
+// pass valuelength prop to maxLength will allow user to past code and allow the autocomplete logic to work.
+
 export default function OTPInput({ value, valueLength, onChange }: Props) {
+   const valueItems = useMemo(() => {
+      const valueArr = value.split("");
+      const items: string[] = [];
+
+      for (let i = 0; i < valueLength; i++) {
+         const char = valueArr[i];
+         if (RE_DIGIT.test(char)) {
+            items.push(char);
+         } else {
+            items.push("");
+         }
+      }
+
+      return items;
+   }, [value, valueLength]);
+
+   const inputOnKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+      const target = e.target as HTMLInputElement;
+
+      if (e.key !== "Backspace" || target.value !== "") {
+         return;
+      }
+
+      const prevElSibiling =
+         (target.previousElementSibling as HTMLInputElement) || null;
+
+      if (prevElSibiling) {
+         prevElSibiling.focus();
+      }
+   };
+
+   const handleInputChange = (
+      e: React.ChangeEvent<HTMLInputElement>,
+      index: number
+   ) => {
+      const target = e.target;
+      let targetValue = target.value;
+      const isTargetValueDigit = RE_DIGIT.test(targetValue);
+
+      if (!isTargetValueDigit && targetValue !== "") {
+         return;
+      }
+
+      targetValue = isTargetValueDigit ? targetValue : " ";
+
+      const newValue =
+         value.substring(0, index) + targetValue + value.substring(index + 1);
+
+      onChange(newValue);
+
+      if (!isTargetValueDigit) {
+         return;
+      }
+
+      const nextElementSibling =
+         target.nextElementSibling as HTMLInputElement | null;
+
+      if (nextElementSibling) {
+         nextElementSibling.focus();
+      }
+   };
+
    return (
       <div className="otp-group">
-         {[1, 2, 4, 5, 6].map((digit, idx) => (
+         {valueItems.map((digit, idx) => (
             <input
                type="text"
                key={idx}
@@ -15,6 +84,8 @@ export default function OTPInput({ value, valueLength, onChange }: Props) {
                inputMode="numeric"
                autoComplete="one-time-code"
                maxLength={valueLength}
+               onChange={(e) => handleInputChange(e, idx)}
+               onKeyDown={(e) => inputOnKeyDown(e)}
                className="otp-input"
                pattern="\d{1}"
             />
